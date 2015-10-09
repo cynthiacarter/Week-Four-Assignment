136c0f8cc3d0f372b79e2107bf199d12c63e56d8
cynthiacarter1 <cynthia.carter@myemail.indwes.edu>
2015-10-09 01:49:36 +0000

    CarterWeekFour

--- piggetty.py
+++ piggetty.py
@@ -1,34 +1,57 @@
-#
-# File Header
-#
-# Add your name to this
+# Piggetty.py 
+# Cynthia Carter
+# IWU Fall 2015 CS 125
+# A program that converts the text to pig latin
 
 # Define a function called piggy(string) that returns a string
-
 def piggy(word):
-	
-	# Magic Happens Here
-	pig = word
-	# Ignore previous line
+	n = 0
+	vowels = "aeiouAEIOU"
+	endword = ""
+	for letter in word:
+		# Check if letter is a vowel
+		if letter in vowels:
+			# True?  We are done
+			if n == 0:
+				pig = word + "yay"
+			break
+		else:
+			endword += word[n]
+			pig = word[n+1:] + endword + "ay"
+		n += 1
 	
 	return pig
 
 # Open the file *getty.txt* for reading.  
+gettyfile = open ("getty.txt","r")
 
 # Open a new file *piggy.txt* for writing.  
+Pmy_file = open ("piggy.txt","w")
 
-# Read the getty.txt file into a string.  
+# Read the getty.txt file into a string. 
+gettystr = gettyfile.read()
 
 # Strip out bad characters (, - .).  
+gettystr = gettystr.replace ('.', '')
+gettystr = gettystr.replace (',', '')
+gettystr = gettystr.replace ('-','')
 
 # Split the string into a list of words.  
+gettylist = gettystr.split()
 
 # Create a new empty string.  
+newstr = ""
 
 # Loop through the list of words, pigifying each one.  
+for word in gettylist:
+	newword = piggy(word)
+	newstr = newstr + newword + " "
+	
+outfile = open("piggy.txt","w")
+print (newstr, gettylist, file=outfile)
+outfile.close()
+gettyfile.close()
 
-# Add the pigified word (and a space) to the new string.  
-
-# Write the new string to piggy.txt.  
+# Write the new string to piggy.txt. 
 
 # close the files.
--- pigify.py
+++ pigify.py
@@ -7,17 +7,24 @@ vowels = "aeiouAEIOU"
 
 # Ask for word
 
+n = 0
+
+
 word = input("Please enter a word: ")
 
 # Loop through word, one letter at a time
 
 for letter in word:
-	# Check if letter is a vowel
+# Check if letter is a vowel
 	if letter in vowels:
 		# True?  We are done
-		pig = word + "yay"
+		if n == 0:
+			pig = word + "yay"
 	else:
+		pig = word[n:] + endword + "ay"
 		# False? Consonant
-		pig = word[1:] + word[0] + "ay"
+		endword += word[n]
+		n += 1
 		
-print(pig)
+
+print(word)
